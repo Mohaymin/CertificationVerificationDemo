6a7ff814c639375c6ee81fb593a339f991401b76
Mohaymin <cabdsrijon@gmail.com>
2026-03-14 00:42:54 -0400

    implement landing screen

--- app/src/main/java/com/example/certificationverificationdemo/MainActivity.kt
+++ app/src/main/java/com/example/certificationverificationdemo/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.certificationverificationdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.certificationverificationdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        
+        binding.issuerBtn.setOnClickListener {
+            Log.d(TAG, "onCreate: issuer button clicked")
+        }
+        binding.verifierBtn.setOnClickListener {
+            Log.d(TAG, "onCreate: verifier button clicked")
+        }
+        binding.holderBtn.setOnClickListener {
+            Log.d(TAG, "onCreate: holder button clicked")
         }
     }
 }
